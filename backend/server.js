8b48e1102b694bed0d4bf6a047616e6a14a945bc
Milky <milkypalle05@.com>
2026-06-12 22:53:08 +0800

    debug db host

--- backend/server.js
+++ backend/server.js
@@ -12,13 +12,18 @@ app.use(cors());
 app.use(express.static("frontend"));
 
 // ✅ DATABASE CONNECTION
+
 const pool = mysql.createPool({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
+  port: process.env.MYSQLPORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
+console.log("DB HOST:", process.env.MYSQLHOST);
 
 // ✅ TEST DATABASE CONNECTION (safe)
 (async () => {
